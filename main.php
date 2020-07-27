2cb6127df9e3c99b7a66357ff8512221ca77bebb
Jeroen Herczeg <jeroen@herczeg.be>
2020-07-27 22:37:20 +0200

    Update main.php

--- main.php
+++ main.php
@@ -4,9 +4,7 @@ print "Starting ...\n";
 print "ENV vars:\n";
 print_r($_ENV);
 print "Fetching http://example.com ...\n";
-$exampleComHtml = file_get_contents('http://example.com');
-var_dump(scandir('/'));
-print "Searching for <h1> tag contents ...\n";
-preg_match_all('/<h1>(.*?)<\/h1>/', $exampleComHtml, $matches);
-print "Found: " . $matches[1][0] . "\n";
+$input = file_get_contents('https://api.apify.com/v2/key-value-stores/' . $_ENV['APIFY_DEFAULT_KEY_VALUE_STORE_ID'] . '/records/' . $_ENV['APIFY_INPUT_KEY']);
+var_dump($input);
+
 print "I am done!\n";
