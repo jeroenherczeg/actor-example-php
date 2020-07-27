1ac671096c70eb1b076c5e301f0414f64eb03a0d
Jeroen Herczeg <jeroen@herczeg.be>
2020-07-27 22:38:32 +0200

    Update main.php

--- main.php
+++ main.php
@@ -5,6 +5,6 @@ print "ENV vars:\n";
 print_r($_ENV);
 print "Fetching http://example.com ...\n";
 $input = file_get_contents('https://api.apify.com/v2/key-value-stores/' . $_ENV['APIFY_DEFAULT_KEY_VALUE_STORE_ID'] . '/records/' . $_ENV['APIFY_INPUT_KEY']);
-var_dump($input);
+var_dump(json_decode($input));
 
 print "I am done!\n";
